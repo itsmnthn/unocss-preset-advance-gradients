69ecb41eb327198ef6325aa96e75e26624536591
Manthankumar Satani <satanimanthan@gmail.com>
2025-03-03 19:06:07 +0530

    fix: unocss config for docs

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,7 +1,7 @@
 import extractorMdc from '@unocss/extractor-mdc'
 import { defineConfig, presetWind3, transformerVariantGroup } from 'unocss'
 import { presetGradientFn } from 'unocss-preset-gradient-fn'
-import presetAdvanceGradients from './src/index.ts'
+import { presetAdvanceGradients } from './src/index.ts'
 
 export default defineConfig({
   presets: [
